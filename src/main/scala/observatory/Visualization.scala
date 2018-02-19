5cde6a25e96bfeb5b7950a3f2206238a581e66dc
chosia <chosia@gosias-MacBook-Pro.local>
2018-02-19 21:50:53 +0100

    Improved the GCD computation - added special handling of equal locations and antipodes. Implemented inverse distance weighting for temp. prediction.

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -2,8 +2,11 @@ package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
 import observatory.Extraction.spark
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.sql.types.{DoubleType, IntegerType, StructField, StructType}
+
 import scala.math._
 
 /**
@@ -25,25 +28,52 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    val rdd = spark.sparkContext.parallelize(temperatures.toList)
-      .map(r => Row(r._1.lat, r._1.lon, r._2))
-    val schema = createLocTempSchema
-    val df = spark.createDataFrame(rdd, schema)
-    predictTemperatureDf(df, location)
+    val rdd: RDD[(Location, Temperature)] = spark.sparkContext.parallelize(temperatures.toList)
+    //  .map(r => Row(r._1.lat, r._1.lon, r._2))
+    //val schema = createLocTempSchema
+    //val df = spark.createDataFrame(rdd, schema)
+    predictTemperatureRdd(rdd, location)
   }
 
   def greatCircleDistance(location1: Location, location2: Location) : Double = {
-    val lat1 = toRadians(location1.lat)
-    val lat2 = toRadians(location2.lat)
-    val lon1 = toRadians(location1.lon)
-    val lon2 = toRadians(location2.lon)
-    val centralAngle = acos(sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2)*cos(lon1 - lon2))
+    val centralAngle =
+      /* Test for the same location */
+      if (location1 == location2)
+        0
+      /* Test for antipodes */
+      else if (location1.lat == -location2.lat && (location1.lon == location2.lon + 180 || location1.lon == location2.lon - 180))
+        Pi
+      else {
+        val lat1 = toRadians(location1.lat)
+        val lat2 = toRadians(location2.lat)
+        val lon1 = toRadians(location1.lon)
+        val lon2 = toRadians(location2.lon)
+        acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2))
+      }
     val earthRadius = 6371.0
     earthRadius * centralAngle
   }
 
-  def predictTemperatureDf(temperatures: DataFrame, location: Location): Temperature = {
-    return 0.0
+  def predictTemperatureRdd(temperatures: RDD[(Location, Temperature)], location: Location): Temperature = {
+    val p = 2.0
+    val tempsWithDistance = temperatures.map(row => {
+      val dist = greatCircleDistance(row._1, location)
+      (row._2, dist)
+    })
+    /* Find the closest point */
+    val (temp, dist) = tempsWithDistance.reduce((a,b) => if (a._2 < b._2) a else b)
+    if (dist < 1.0) {
+      /* If the smallest distance is less than 1km, return the temperature at this closest point */
+      temp
+    } else {
+      /* Otherwise apply inverse distance weighting */
+      val weightedTemps = tempsWithDistance.map(row => {
+        val weight = 1.0 / pow(row._2, p)
+        (row._1 * weight, weight)
+      })
+      val (a, b) = weightedTemps.reduce((a, b) => (a._1 + b._1, a._2 + b._2))
+      a / b
+    }
   }
 
 
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -6,26 +6,66 @@ import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
 
+
 @RunWith(classOf[JUnitRunner])
 class VisualizationTest extends FunSuite with Checkers {
+
+  def roundScale(x: Double, scale: Int): Double = {
+    BigDecimal(x).setScale(1, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
 
   test("test Great Circle Distance") {
     val locationPairs = List(
       (Location(50.0, 0.0), Location(58.0, 0.0)),
       (Location(52.0, 4.0), Location(52.0, 21.0)),
-      (Location(40.7, -74.0), Location(52.0, 4.0))
+      (Location(40.7, -74.0), Location(52.0, 4.0)),
+      (Location(52.32, 12.34), Location(52.32, 12.34)),
+      (Location(90,0), Location(-90,0)),
+      (Location(90,0), Location(-90, 180)),
+      (Location(51.12, -12.0), Location(-51.12, -12.0+180))
     )
-    val distances = List(889.6, 1161.1, 5820.5)
+    val antipodesDistance = roundScale(math.Pi*6371, 1)
+    val distances = List(889.6, 1161.1, 5820.5, 0, antipodesDistance, antipodesDistance, antipodesDistance)
 
     val computedDistances = locationPairs.map(pair => {
       val dist = Visualization.greatCircleDistance(pair._1, pair._2)
-      BigDecimal(dist).setScale(1, BigDecimal.RoundingMode.HALF_UP).toDouble
+      roundScale(dist, 1)
     })
 
     val pairedDistances = distances.zip(computedDistances)
 
-    pairedDistances.foreach(pair => assert(pair._1 == pair._2))
+    pairedDistances.foreach(pair => {
+      println(s"Testing pair (${pair._1}, ${pair._2})")
+      assert(pair._1 == pair._2)
+    })
 
+  }
+
+  test("test spatial interpolation - single point") {
+    val location = Location(50.0, 4.0)
+    val temp = 32.1
+    val points = List((location, temp))
+    val res = Visualization.predictTemperature(points, location)
+    assert(res == temp)
+  }
+
+  test("test spacial interpolation - one point equal") {
+    val location = Location(-52.0, -10.0)
+    val temp = 10.1
+    val points = List((location, temp), (Location(32,32), -10.0))
+    val res = Visualization.predictTemperature(points, location)
+    assert(res == temp)
+  }
+
+  test("test spacial interpolation - half way through") {
+    val loc1 = Location(52.0, 10.0)
+    val loc2 = Location(52.0, 20.0)
+    val temp1 = 10.0
+    val temp2 = 20.0
+    val testloc = Location(52.0, 15.0)
+    val points = List((loc1, temp1), (loc2, temp2))
+    val res = Visualization.predictTemperature(points, testloc)
+    assert(roundScale(res,1) == 15.0)
   }
 
 }
